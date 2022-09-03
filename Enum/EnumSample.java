eb5045fce851e0790e377487f49747f61e142b9f
JoowonSuh <sjw2023@naver.com>
2022-09-03 13:44:41 +0900

    Enum example

---
+++ Enum/EnumSample.java
@@ -0,0 +1,13 @@
+enum color{
+    RED,
+    BLUE,
+    YELLOW,
+    GREEN
+}
+
+public class EnumSample{
+    public static void main (String [] args ){
+        color red = color.RED;
+        System.out.println(red);
+    }
+}
